b13e7ef6e048ab854e29c6f369882c907a1de490
Vadim Osochenko <rokerez@yandex.ua>
2017-08-20 17:09:41 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use GuzzleHttp\Client;
 
 Route::match(['get', 'post'], '/botman', 'BotManController@handle');
+
+Route::get('/test', function () {
+    $client = new Client();
+    $uri = 'https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11';
+    $response = $client->get($uri);
+    $results = json_decode($response->getBody()->getContents());
+    $date = date('d F Y', strtotime($results->date));
+    $data = "Here's the exchange rates based on $currency currency\nDate: $date \n";
+
+    foreach ($results->rates as $k => $v) {
+        $data .= $k . " - " . $v . "\n";
+    }
+
+    return $data;
+});
