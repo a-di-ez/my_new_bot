992e34566c926ab3542b9834c446171d4327a407
Vadim Osochenko <rokerez@yandex.ua>
2017-08-20 15:37:08 +0300

    first

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Conversations\ExampleConversation;
+use Illuminate\Http\Request;
+use Mpociot\BotMan\BotMan;
+use GuzzleHttp\Client;
+
+class BotManController extends Controller
+{
+    public function handle()
+    {
+        $botman = app('botman');
+
+        // Simple respond method
+        $botman->hears('Привет', function (BotMan $bot) {
+            $bot->types();
+            $bot->reply('Здрасте:) Как вас величать?');
+        });
+
+        $botman->hears('Я {name}', function (BotMan $bot, $name) {
+            $bot->types();
+            $bot->reply('Очень приятно, ' . $name . ':) Что вы здесь забыли?');
+        });
+
+        $botman->hears('Give me {currency} rates', function (BotMan $bot, $currency) {
+            $bot->types();
+            $results = $this->getCurrency($currency);
+            $bot->reply($results);
+        });
+
+        $botman->fallback(function (BotMan $bot) {
+            $bot->types();
+            $bot->reply('Моя твоя непонимать...');
+        });
+
+        $botman->listen();
+    }
+
+    public function getCurrency($currency)
+    {
+        $client = new Client();
+        $uri = 'http://api.fixer.io/latest?base=' . $currency;
+        $response = $client->get($uri);
+        $results = json_decode($response->getBody()->getContents());
+        $date = date('d F Y', strtotime($results->date));
+        $data = "Here's the exchange rates based on $currency currency\nDate: $date \n";
+
+        foreach ($results->rates as $k => $v) {
+            $data .= $k . " - " . $v . "\n";
+        }
+
+        return $data;
+    }
+}
